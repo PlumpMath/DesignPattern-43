9cce3c951adadeed6b5ffd65a6ef04df503a6abc
MattCao <mattycao@gmail.com>
2014-04-14 00:15:20 -0700

    Finish the Observer Pattern

---
+++ 2ObserverPattern/selfMade/WeatherData.java
@@ -0,0 +1,52 @@
+ import java.util.ArrayList;
+ public class WeatherData implements Subject {
+ 	private ArrayList<Observer> observers;
+ 	private float temperature;
+ 	private float humidity;
+ 	private float pressure;
+
+ 	public WeatherData() {
+ 		observers = new ArrayList<Observer>();
+ 	}
+
+ 	public void registerObserver(Observer o) {
+ 		observers.add(o);
+ 	}
+
+ 	public void removeObserver(Observer o) {
+ 		int i = observers.indexOf(o);
+ 		if(i >= 0)
+ 			observers.remove(i);
+ 	}
+
+ 	public void notifyObservers() {
+ 		for(int i = 0; i < observers.size(); i++) {
+ 			@SuppressWarnings("unchecked")
+ 			Observer observer = (Observer)observers.get(i);
+ 			observer.update(temperature, humidity, pressure);
+ 		}
+ 	}
+
+ 	public void measurementsChanged() {
+ 		notifyObservers();
+ 	}
+
+ 	public void setMeasurements(float temperature, float humidity, float pressure) {
+ 		this.temperature = temperature;
+ 		this.humidity = humidity;
+ 		this.pressure = pressure;
+ 		measurementsChanged();
+ 	}
+
+ 	public float getTemperature() {
+ 		return this.temperature;
+ 	}
+
+ 	public float getHumidity() {
+ 		return this.humidity;
+ 	}
+
+ 	public float getPressure() {
+ 		return this.humidity;
+ 	}
+ }
